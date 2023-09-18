16e35c48735ce3a189310184d05346c526114fac
Clifftek <jakob.johnson.dev@gmail.com>
2023-09-18 21:10:16 +1000

    finished billboard entity

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -6,7 +6,9 @@ type HeadingProps = {
 export default function Heading({ title, description }: HeadingProps) {
   return (
     <div>
-      <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+      <h2 className='text-3xl font-bold tracking-tight text-[#A821EE]'>
+        {title}
+      </h2>
       <p className='text-sm text-muted-foreground'>{description}</p>
     </div>
   );
